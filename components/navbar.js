b936bc4dddd7f38738b39a597228f997436bb3ce
bluelf45 <ohab0408@gmail.com>
2023-09-02 14:55:47 -0400

    Nextjs

---
+++ components/navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                  <p>About Us</p>
+
+              </li>
+              <li>
+                <p>Cringe</p>
+              </li>
+              <li>
+              <p>Cringe</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
