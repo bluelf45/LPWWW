6c7bc3976b6c6204f9065631b03d7dfc44925de9
bluelf45 <ohab0408@gmail.com>
2023-09-03 19:04:52 -0300

    sis

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 				style={{ position: 'absolute', bottom: 0, width: '100%' }}
 				className='mb-auto text-center text-lg-start bg-light text-muted'
 			>
-				Matame porfavor no se como hacer que esto este abajo :(
+				EL FOOTER ESTA ABAJO NO PUEDE SER
 			</footer>
 		</>
 	);
