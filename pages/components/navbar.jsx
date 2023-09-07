3da17425dc1fe83bd2039cfb287b96c73e1e37b0
bluelf45 <ohab0408@gmail.com>
2023-09-06 21:32:14 -0300

    holi

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -1,42 +1,43 @@
 import React from 'react'; // import react module
 import 'bootstrap/dist/css/bootstrap.css';
+import Link from 'next/link';
+
 const Navbar = () => {
 	return (
 		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
 			<div className='container-fluid'>
-				<a className='navbar-brand' href='#'>
+				<a className='navbar-brand' href='/'>
 					Navbar
 				</a>
 				<button
 					className='navbar-toggler'
 					type='button'
 					data-bs-toggle='collapse'
-					data-bs-target='#navbarNav'
-					aria-controls='navbarNav'
+					data-bs-target='#Navbar'
 					aria-expanded='false'
 					aria-label='Toggle navigation'
 				>
 					<span className='navbar-toggler-icon'></span>
 				</button>
-				<form className='me-auto'>
-					<ul className='navbar-nav'>
+				<form className='collapse navbar-collapse me-auto' id='Navbar'>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
 						<li className='nav-item'>
-							<a className='nav-link active' aria-current='page' href='/'>
-								Home
-							</a>
-						</li>
-						<li className='nav-item'>
-							<a className='nav-link' href='Solicitudes'>
+							<Link className='nav-link' href='/Solicitudes'>
 								Solicitudes
-							</a>
+							</Link>
 						</li>
 						<li className='nav-item'>
-							<a className='nav-link' href='#'>
-								Pricing
+							<Link className='nav-link' href='/'>
+								Inventario
+							</Link>
+						</li>
+						<li>
+							<a className='nav-link' href='/usuarios' tabIndex='-1' aria-disabled='true'>
+								Usuarios
 							</a>
 						</li>
-						<li className='ms-auto'>
-							<a className='nav-link d-flex' href='Perfil' tabIndex='-1' aria-disabled='true'>
+						<li>
+							<a className='nav-link' href='/perfil' tabIndex='-1' aria-disabled='true'>
 								Perfil
 							</a>
 						</li>
