ec7fee251b586837984d853edca5b516618edc46
matiasgonzalezz0 <matiasgonzalezz.mail@gmail.com>
2023-09-07 19:54:38 -0300

    arreglo problema scripts

--- src/pages/Inventario/index.jsx
+++ src/pages/Inventario/index.jsx
@@ -1,10 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import Layout from '@/components/Layout';
-import styles from './index.module.css';
 
 export default function Inventario() {
 	return (
 		<Layout>
-			<a className={styles.title}>Inventario</a>
+			<div className='d-flex'>
+				<h2>Inventario</h2>
+				<div className='dropdown'>
+					<button
+						className='btn dropdown-toggle'
+						type='button'
+						id='dropdownMenuButton'
+						data-toggle='dropdown'
+						aria-haspopup='true'
+						aria-expanded='false'
+					>
+						Tipo de producto
+					</button>
+					<div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+						<a className='dropdown-item' href='#'>
+							Materiales
+						</a>
+						<a className='dropdown-item' href='#'>
+							Herramientas
+						</a>
+						<a className='dropdown-item' href='#'>
+							Equipos
+						</a>
+					</div>
+				</div>
+			</div>
 		</Layout>
 	);
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,8 @@
-import Script from 'next/script';
 import Home from './Home';
 
 export default function App() {
 	return (
 		<>
-			<Script
-				src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js'
-				integrity='sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3'
-				crossOrigin='anonymous'
-			/>
-			<Script
-				src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js'
-				integrity='sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V'
-				crossOrigin='anonymous'
-			/>
 			<Home />
 		</>
 	);
