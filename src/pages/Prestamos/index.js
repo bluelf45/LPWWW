7777be7b34e0184cc3228d5cd1b6464e45129dd1
bluelf45 <ohab0408@gmail.com>
2023-09-09 11:14:58 -0300

    prestamos commit message ```

--- src/pages/Prestamos/index.js
+++ src/pages/Prestamos/index.js
@@ -1,14 +1,12 @@
 import Layout from '@/components/Layout';
+// import styles from './index.module.css';
 
 export default function Perfil() {
 	return (
 		<Layout>
 			<h2>Prestamos y Devoluciones</h2>
-			<div className='container'>
-				<div className='row'>
-					<div className='col-sm'> Prestamos</div>
-					<div className='col-sm'> Devoluciones</div>
-				</div>
+			<div className='row'>
+				<div className='col-sm'> Prestamos</div>
 			</div>
 		</Layout>
 	);
